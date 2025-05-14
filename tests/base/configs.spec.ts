22fdcc42eec787a11956df1de0b3ec5f86d116cc
Bruno Fernando Bevilaqua <bruno.f.bevilaqua@gmail.com>
2025-05-14 16:24:07 -0300

    add negative stock test

--- tests/base/configs.spec.ts
+++ tests/base/configs.spec.ts
@@ -10,16 +10,29 @@ test('Put .pfx in account', async ({ page }) => {
   await page.getByRole('heading', { name: 'Configurações gerais' }).click();
   let hasCertificate = await page.getByRole('textbox', { name: 'Emitido para' }).isVisible();
 
-  if(hasCertificate){
+  if (hasCertificate) {
     await page.getByRole('button').filter({ hasText: 'close' }).click();
     await page.getByRole('button', { name: 'Remover' }).click();
   }
 
   await page.getByRole('button', { name: 'Carregar certificado' }).click();
-  await page.locator('input[type="file"][name="certFile"]').setInputFiles('tests/assets/certificate/'+process.env.PLAYWRIGHT_CERT_FILE);
+  await page.locator('input[type="file"][name="certFile"]').setInputFiles('tests/assets/certificate/' + process.env.PLAYWRIGHT_CERT_FILE);
   await page.getByRole('textbox', { name: 'Senha do certificado (' }).click();
   await page.getByRole('textbox', { name: 'Senha do certificado (' }).fill(process.env.PLAYWRIGHT_CERT_PASSWORD || '');
   await page.getByRole('button', { name: 'Enviar' }).click();
 
   await expect.soft(page.locator('gw-general-settings')).toContainText('Emitido para');
+});
+
+test('Allow negative stock', async ({ page }) => {
+  await page.getByRole('button', { name: 'Configurações' }).click();
+  await page.getByRole('link', { name: 'Geral' }).click();
+  await page.getByRole('heading', { name: 'Configurações gerais' }).click();
+  let allow_negative_stock = await page.locator('.mat-checkbox-inner-container').first().isChecked();
+  if (!allow_negative_stock) {
+    await page.locator('label').filter({ hasText: 'Permitir estoque negativo' }).click();
+    await page.locator('mat-card-actions').filter({ hasText: 'Desfazer Salvar' }).getByRole('button').nth(1).click();
+  }
+  allow_negative_stock = await page.locator('.mat-checkbox-inner-container').first().isChecked();
+  expect(allow_negative_stock).toEqual(true);
 });
