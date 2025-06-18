5bea2713e2e4ea05d583543c7cb6f9bd0bcbde41
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2025-06-18 10:09:01 -0300

    added some validations to create, edit and delete functions

--- tests/registers/vehicles.spec.ts
+++ tests/registers/vehicles.spec.ts
@@ -1,9 +1,59 @@
 import { test } from '../helpers/fixtures';
-import { expect } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import { generate as generateCnpj, format as formatCnpj } from 'cnpj';
+import { generate as generateCPF } from 'gerador-validador-cpf';
 
-const vehicle1 = {
+type Vehicle = {
+  description: string;
+  plate: string;
+  uf: string;
+  rntrc: string;
+  renavam: string;
+  tara: string;
+  cap_m3: string;
+  cap_kg: string;
+  wheelset_type: "Utilitário";
+  car_body_type: "Aberta";
+  axes_quantity: string;
+  owner: Owner; // Pode ser um objeto Owner ou uma string com o nome do proprietário
+}
+
+type Owner = {
+    name: string;
+    surname?: string;
+    national_document?: string;
+    state_document?: string;
+    postal_code?: string;
+    local?: string;
+    district?: string;
+    number?: string;
+    country?: string;
+    state?: string;
+    city_name?: string;
+    phone?: string;
+    cell?: string;
+    email?: string;
+  }
+
+const owner: Owner = {
+  name: "Zucchetti (Bruno Fernando)",
+  surname: "Zucchetti (Bruno Fernando)",
+  national_document: generateCPF(),
+  state_document: faker.string.numeric({ length: 7 }),
+  postal_code: '89700-055',
+  local: 'Rua Marechal Deodoro',
+  district: 'Centro',
+  number: '1280',
+  country: 'Brasil',
+  state: 'SC',
+  city_name: 'Concórdia',
+  phone: '4934414120',
+  cell: '(49) 9200-11913',
+  email: faker.internet.email(),
+}
+
+const vehicle1: Vehicle = {
   description: "Auto " + faker.vehicle.vehicle(),
   plate: faker.word.sample({ length: 3}) + "-" + faker.string.numeric({ length: 4 }),
   uf: "SC",
@@ -15,10 +65,10 @@ const vehicle1 = {
   wheelset_type: "Utilitário",
   car_body_type: "Aberta",
   axes_quantity: faker.string.numeric({length: 1, allowLeadingZeros: false}),
-  owner: "Zucchetti (Bruno Fernando)"
+  owner: { name: "Zucchetti (Bruno Fernando)"}
 };
 
-const vehicle2 = {
+const vehicle2: Vehicle = {
   description: "Auto " + faker.vehicle.vehicle(),
   plate: faker.word.sample({ length: 3}) + "-" + faker.string.numeric({ length: 4 }),
   uf: "SC",
@@ -48,6 +98,8 @@ const vehicle2 = {
   }
 };
 
+var vehicles: Vehicle[] = [];
+
 test('Create a first new Vehicle', async ({ page }) => {
   // acessando o menu
   await page.getByRole('button', { name: 'Cadastros' }).click();
@@ -73,7 +125,13 @@ test('Create a first new Vehicle', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Proprietário' }).click();
   await page.getByRole('textbox', { name: 'Proprietário' }).fill('Bruno Fernando');
   await page.getByRole('button').filter({ hasText: 'search' }).click();
-  await page.getByText('Zucchetti (Bruno Fernando) (').click();
+  await page.waitForTimeout(2000);
+  // verifica se o proprietário já existe
+  if(!(await page.getByText('Zucchetti (Bruno Fernando)').isVisible())) {
+    await AddOwner(page);
+  } else {
+    await page.getByText('Zucchetti (Bruno Fernando)').click();
+  }
 
   // informações do veículo
   await page.getByRole('textbox', { name: 'Tara (Kg)' }).click();
@@ -95,9 +153,10 @@ test('Create a first new Vehicle', async ({ page }) => {
 
   // verifica se foi criado
   await page.getByRole('searchbox', { name: 'Digite para buscar...' }).fill(vehicle1.description);
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(3000);
   let isRegistered = await page.getByRole('heading', { name: new RegExp('(' + vehicle1.description + ')') }).isVisible();
   await expect(isRegistered).toBe(true);
+  vehicles.push(vehicle1);
 });
 
 test('Create a second new Vehicle', async ({ page }) => {
@@ -128,54 +187,55 @@ test('Create a second new Vehicle', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Nome', exact: true }).click();
   await page.getByRole('textbox', { name: 'Nome', exact: true }).fill(vehicle2.owner.name);
   await page.getByRole('textbox', { name: 'Nome Fantasia' }).click();
-  await page.getByRole('textbox', { name: 'Nome Fantasia' }).fill(vehicle2.owner.surname);
+  await page.getByRole('textbox', { name: 'Nome Fantasia' }).fill(vehicle2.owner.surname!);
   await page.getByRole('textbox', { name: 'CNPJ' }).click();
-  await page.getByRole('textbox', { name: 'CNPJ' }).fill(vehicle2.owner.national_document);
+  await page.getByRole('textbox', { name: 'CNPJ' }).fill(vehicle2.owner.national_document!);
   await page.getByRole('textbox', { name: 'IE' }).click();
-  await page.getByRole('textbox', { name: 'IE' }).fill(vehicle2.owner.state_document);
+  await page.getByRole('textbox', { name: 'IE' }).fill(vehicle2.owner.state_document!);
   await page.getByRole('textbox', { name: 'CEP' }).click();
-  await page.getByRole('textbox', { name: 'CEP' }).fill(vehicle2.owner.postal_code);
+  await page.getByRole('textbox', { name: 'CEP' }).fill(vehicle2.owner.postal_code!);
   await page.getByRole('textbox', { name: 'CEP' }).press('Tab');
   await page.getByRole('textbox', { name: 'Número' }).click();
-  await page.getByRole('textbox', { name: 'Número' }).fill(vehicle2.owner.number);
+  await page.getByRole('textbox', { name: 'Número' }).fill(vehicle2.owner.number!);
   await page.getByRole('textbox', { name: 'Telefone' }).click();
-  await page.getByRole('textbox', { name: 'Telefone' }).fill(vehicle2.owner.phone);
+  await page.getByRole('textbox', { name: 'Telefone' }).fill(vehicle2.owner.phone!);
   await page.getByRole('textbox', { name: 'Celular' }).click();
-  await page.getByRole('textbox', { name: 'Celular' }).fill(vehicle2.owner.cell);
+  await page.getByRole('textbox', { name: 'Celular' }).fill(vehicle2.owner.cell!);
   await page.getByRole('textbox', { name: 'E-mail' }).click();
-  await page.getByRole('textbox', { name: 'E-mail' }).fill(vehicle2.owner.email);
+  await page.getByRole('textbox', { name: 'E-mail' }).fill(vehicle2.owner.email!);
   // verifica o endereço do proprietário
-  await expect.soft(page.getByRole('textbox', { name: 'Logradouro' })).toHaveValue(vehicle2.owner.local);
-  await expect.soft(page.getByRole('textbox', { name: 'Bairro' })).toHaveValue(vehicle2.owner.district);
-  await expect.soft(page.getByRole('combobox', { name: 'País' })).toHaveValue(vehicle2.owner.country)
-  await expect.soft(page.getByRole('combobox', { name: 'UF' })).toHaveValue(vehicle2.owner.state);
-  await expect.soft(page.getByRole('combobox', { name: 'Município' })).toHaveValue(vehicle2.owner.city_name);
+  await expect.soft(page.getByRole('textbox', { name: 'Logradouro' })).toHaveValue(vehicle2.owner.local!);
+  await expect.soft(page.getByRole('textbox', { name: 'Bairro' })).toHaveValue(vehicle2.owner.district!);
+  await expect.soft(page.getByRole('combobox', { name: 'País' })).toHaveValue(vehicle2.owner.country!)
+  await expect.soft(page.getByRole('combobox', { name: 'UF' })).toHaveValue(vehicle2.owner.state!);
+  await expect.soft(page.getByRole('combobox', { name: 'Município' })).toHaveValue(vehicle2.owner.city_name!);
   // cadastra o proprietário
   await page.getByRole('button', { name: 'Confirmar' }).click();
 
   // informações do veículo
   await page.getByRole('textbox', { name: 'Tara (Kg)' }).click();
-  await page.getByRole('textbox', { name: 'Tara (Kg)' }).fill(vehicle1.tara);
+  await page.getByRole('textbox', { name: 'Tara (Kg)' }).fill(vehicle2.tara);
   await page.getByRole('textbox', { name: 'Capacidade (Kg)' }).click();
-  await page.getByRole('textbox', { name: 'Capacidade (Kg)' }).fill(vehicle1.cap_kg);
+  await page.getByRole('textbox', { name: 'Capacidade (Kg)' }).fill(vehicle2.cap_kg);
   await page.getByRole('textbox', { name: 'Capacidade (m³)' }).click();
-  await page.getByRole('textbox', { name: 'Capacidade (m³)' }).fill(vehicle1.cap_m3);
+  await page.getByRole('textbox', { name: 'Capacidade (m³)' }).fill(vehicle2.cap_m3);
   await page.getByLabel('Tipo de rodado').getByText('Tipo de rodado').click();
-  await page.getByText(vehicle1.wheelset_type).click();
+  await page.getByText(vehicle2.wheelset_type).click();
   await page.getByLabel('Tipo de carroceria').getByText('Tipo de carroceria').click();
-  await page.getByText(vehicle1.car_body_type).click();
+  await page.getByText(vehicle2.car_body_type).click();
   await page.getByRole('textbox', { name: 'Quantidade de eixos' }).click();
-  await page.getByRole('textbox', { name: 'Quantidade de eixos' }).fill(vehicle1.axes_quantity);
+  await page.getByRole('textbox', { name: 'Quantidade de eixos' }).fill(vehicle2.axes_quantity);
 
   //salva o veículo
   await page.getByRole('button', { name: 'Salvar' }).click();
   await page.locator('h1').filter({ hasText: 'Veículos' }).click();
 
   // verifica se foi criado
-  await page.getByRole('searchbox', { name: 'Digite para buscar...' }).fill(vehicle1.description);
+  await page.getByRole('searchbox', { name: 'Digite para buscar...' }).fill(vehicle2.description);
   await page.waitForTimeout(2000);
-  let isRegistered = await page.getByRole('heading', { name: new RegExp('(' + vehicle1.description + ')') }).isVisible();
+  let isRegistered = await page.getByRole('heading', { name: new RegExp('(' + vehicle2.description + ')') }).isVisible();
   await expect(isRegistered).toBe(true);
+  vehicles.push(vehicle2);
 });
 
 test('Edit Vehicle1', async ({ page }) => {
@@ -183,31 +243,36 @@ test('Edit Vehicle1', async ({ page }) => {
   await page.getByRole('button', { name: 'Cadastros' }).click();
   await page.getByRole('link', { name: 'Veículos' }).click();
 
+  if(vehicles.length === 0) {
+    throw new Error("No vehicles to edit. Please create vehicles first.");
+  }
+
   //pesquisa o veículo e clica em editar
   await page.locator('div').filter({ hasText: /^Digite para buscar\.\.\.$/ }).click();
-  await page.getByRole('searchbox', { name: 'Digite para buscar...' }).fill(vehicle1.description);
-  await page.getByRole('heading', { name: new RegExp('(' + vehicle1.description + ')') }).click();
+  await page.getByRole('searchbox', { name: 'Digite para buscar...' }).fill(vehicles[0].description);
+  await page.waitForTimeout(2000);
+  await page.getByRole('heading', { name: new RegExp('(' + vehicles[0].description + ')') }).click();
   await page.getByRole('button', { name: 'Editar' }).click();
 
-  vehicle1.description = "Auto " + faker.vehicle.vehicle();
-  vehicle1.tara = faker.string.numeric({ length: 4, allowLeadingZeros: false });
-  vehicle1.cap_m3 = faker.string.numeric({ length: 4, allowLeadingZeros: false });
+  vehicles[0].description = "Auto " + faker.vehicle.vehicle();
+  vehicles[0].tara = faker.string.numeric({ length: 4, allowLeadingZeros: false });
+  vehicles[0].cap_m3 = faker.string.numeric({ length: 4, allowLeadingZeros: false });
 
   // muda os dados no formulário
   await page.getByRole('textbox', { name: 'Descrição' }).click();
-  await page.getByRole('textbox', { name: 'Descrição' }).fill(vehicle1.description);
+  await page.getByRole('textbox', { name: 'Descrição' }).fill(vehicles[0].description);
   await page.getByRole('textbox', { name: 'Tara (Kg)' }).click();
-  await page.getByRole('textbox', { name: 'Tara (Kg)' }).fill(vehicle1.tara);
+  await page.getByRole('textbox', { name: 'Tara (Kg)' }).fill(vehicles[0].tara);
   await page.getByRole('textbox', { name: 'Capacidade (Kg)' }).click();
-  await page.getByRole('textbox', { name: 'Capacidade (Kg)' }).fill(vehicle1.cap_m3);
+  await page.getByRole('textbox', { name: 'Capacidade (Kg)' }).fill(vehicles[0].cap_m3);
 
   //salva o veículo
   await page.getByRole('button', { name: 'Salvar' }).click();
 
   // verifica se foi criado
-  await page.getByRole('searchbox', { name: 'Digite para buscar...' }).fill(vehicle1.description);
+  await page.getByRole('searchbox', { name: 'Digite para buscar...' }).fill(vehicles[0].description);
   await page.waitForTimeout(2000);
-  let isRegistered = await page.getByRole('heading', { name: new RegExp('(' + vehicle1.description + ')') }).isVisible();
+  let isRegistered = await page.getByRole('heading', { name: new RegExp('(' + vehicles[0].description + ')') }).isVisible();
   await expect(isRegistered).toBe(true);
 });
 
@@ -215,13 +280,12 @@ test('Delete Vehicles', async ({ page }) => {
   // acessa o menu
   await page.getByRole('button', { name: 'Cadastros' }).click();
   await page.getByRole('link', { name: 'Veículos' }).click();
-  
-  // cria uma lista de veículos
-  let vehicles = [
-    vehicle1,
-    vehicle2
-  ];
 
+  // verifica se existem veículos cadastrados
+  if (vehicles.length === 0) {
+    throw new Error("No vehicles to delete. Please create vehicles first.");
+  }
+  
   for (let i = 0; i < vehicles.length; i++) {
     const vehicle = vehicles[i];
       // pesquisa o veículo
@@ -239,3 +303,31 @@ test('Delete Vehicles', async ({ page }) => {
       await expect(page.locator('h3')).toContainText('Nada encontrado com "'+vehicle.description+'"');
   }
 });
+
+async function AddOwner(page: Page) {
+  await page.getByRole('button', { name: 'Cadastrar pessoa' }).click();
+    await page.getByRole('textbox', { name: 'Nome' }).click();
+    await page.getByRole('textbox', { name: 'Nome' }).fill(owner.name);
+    await page.getByRole('textbox', { name: 'Apelido' }).click();
+    await page.getByRole('textbox', { name: 'Apelido' }).fill(owner.surname!);
+    await page.getByRole('textbox', { name: 'CPF' }).click();
+    await page.getByRole('textbox', { name: 'CPF' }).fill(owner.national_document!);
+    await page.getByRole('textbox', { name: 'RG' }).click();
+    await page.getByRole('textbox', { name: 'RG' }).fill(owner.state_document!);
+    await page.getByRole('textbox', { name: 'CEP' }).click();
+    await page.getByRole('textbox', { name: 'CEP' }).fill(owner.postal_code!);
+    await page.getByRole('textbox', { name: 'Logradouro' }).click();
+    await page.getByRole('textbox', { name: 'Logradouro' }).fill(owner.local!);
+    await page.getByRole('textbox', { name: 'Número' }).click();
+    await page.getByRole('textbox', { name: 'Número' }).fill(owner.number!);
+    await page.getByRole('textbox', { name: 'Complemento' }).click();
+    await page.getByRole('textbox', { name: 'Complemento' }).fill('Casa 1');
+    await page.getByRole('textbox', { name: 'Telefone' }).click();
+    await page.getByRole('textbox', { name: 'Telefone' }).fill(owner.phone!);
+    await page.getByRole('textbox', { name: 'Celular' }).click();
+    await page.getByRole('textbox', { name: 'Celular' }).fill(owner.cell!);
+    await page.getByRole('textbox', { name: 'E-mail' }).click();
+    await page.getByRole('textbox', { name: 'E-mail' }).fill(owner.email!);
+    await page.getByLabel('Cadastrar pessoa').getByRole('button', { name: 'Confirmar' }).click();
+    await expect(page.getByText('Pessoa cadastrada com sucesso').isVisible()).toBeTruthy();
+}
