56aa39f9a66e3f5a217a435c1fe183a34731100b
Bruno Fernando Bevilaqua <bruno.f.bevilaqua@gmail.com>
2025-07-08 14:51:04 -0300

    update avoid block screen

--- tests/new-accounts/2-preferences.spec.ts
+++ tests/new-accounts/2-preferences.spec.ts
@@ -5,5 +5,10 @@ test('Should set block screen to "never"', async ({ page }) => {
   await page.goto(process.env.PLAYWRIGHT_GWEB_URL + "/usuario/preferencias");
   await page.waitForURL(process.env.PLAYWRIGHT_GWEB_URL + "/usuario/preferencias");
   await page.getByRole('heading', { name: 'Preferências do usuário' }).click();
+
+  await page.locator('mat-select[id="preferences.lock_screen_timeout"]').click();
   
+  //await page.getByRole('combobox', { name: '15 minutos' });
+  //await page.getByRole('combobox').click();
+  await page.getByRole('option', { name: 'Nunca' }).click();
 });
